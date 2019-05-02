d4929c7e86aa48a0ce2680888fc80d2d30e6069e
alkateb <36197396+alkateb@users.noreply.github.com>
2019-05-02 16:30:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -160,8 +160,8 @@ $speech = $e;
      
   
     $response = new \stdClass();
-    $response->fulfillmentText->text=$speech;
-    $response->fulfillmentMessages->text= $speech;
+    $response->fulfillmentText = $speech;
+    $response->fulfillmentMessages[]= $speech;
     $response->source = "webhook";
     $u= json_encode($response);
 	//$u=str_replace('\','',$u);
